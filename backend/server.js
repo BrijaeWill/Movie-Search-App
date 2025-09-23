0076a38cf36173481fb5ab9429be8c6317240042
BrijaeWil <brijaewilliams5@gmail.com>
2025-09-23 14:05:00 -0400

    redid files for backend

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,8 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//routes
-app.get("/", (req, res) => {
+// Simple test middleware route
+app.get("/", (_req, res) => {
   res.send(" Server is running!");
 });
 
