baeb20fade2987a526fe9ae8d694d4aa25ecff78
BrijaeWil <brijaewilliams5@gmail.com>
2025-09-30 22:33:54 -0400

    fixing mistake

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-import moviesRouter from "./routes/movies.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,8 +12,7 @@ app.use(express.json());
 app.get("/", (_req, res) => {
   res.send(" Server is running!");
 });
-//Movie routes
-app.use("/api/movies", moviesRouter);
+
 // Start server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
